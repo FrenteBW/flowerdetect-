c649adfd28434e0a489ed048a80f8029516d3736
An Byungwook <abw2619@icloud.com>
2022-05-07 15:52:58 +0900

    First Commit

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'flowerdetect!' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for flowerdetect!
+
+pod 'Alamofire', '~> 4.4'
+pod 'SwiftyJSON'
+pod 'SDWebImage'
+
+end
--- flowerdetect!/ViewController.swift
+++ flowerdetect!/ViewController.swift
@@ -1,19 +1,113 @@
-//
-//  ViewController.swift
-//  flowerdetect!
-//
-//  Created by 안병욱의 mac on 2022/05/07.
-//
-
 import UIKit
+import CoreML
+import Vision
+import Alamofire
+import SwiftyJSON
+import SDWebImage
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let wikipediaURL = "https://en.wikipedia.org/w/api.php"
+    let imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = true //crop the image
+        
     }
-
-
+    
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
+        //edited image = cropped image
+            guard let convertedCIImage = CIImage(image: userPickedImage) else{ //CIImage로 변환
+                
+                fatalError("cannot convert to CIImage.")
+                
+            }
+            
+            detect(image: convertedCIImage)
+        
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    //
+    func detect(image: CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else{ //학습된 모델 불러오기
+            
+            fatalError("Cannot import model")
+            
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in //모델 request 불러오기
+                    guard let result = request.results?.first as? VNClassificationObservation else {
+                        fatalError("Could not complete classfication")
+                    }
+              
+                    self.navigationItem.title = result.identifier.capitalized //Flower name 보여주기
+                    self.requestInfo(flowerName: result.identifier)
+                    
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image) //모델 handler 불러오기
+        
+        do{
+        try handler.perform([request])
+        }
+        
+        catch{
+            print(error)
+        }
+    }
+    
+    func requestInfo(flowerName: String){
+        
+        let parameters : [String:String] = [
+            "format" : "json",
+            "action" : "query",
+            "prop" : "extracts|pageimages",
+            "exintro" : "",
+            "explaintext" : "",
+            "titles" : flowerName,
+            "redirects" : "1",
+            "pithumbsize" : "500",
+            "indexpageids" : ""
+            
+        ]
+        
+        Alamofire.request(wikipediaURL, method: .get, parameters: parameters).responseJSON
+        { (response) in
+            if response.result.isSuccess{
+                
+                let flowerJSON : JSON = JSON(response.result.value!)
+                
+                let pageid = flowerJSON["query"]["pageids"][0].stringValue
+                
+                let flowerDescription = flowerJSON["query"]["pages"][pageid]["extract"].stringValue //설명부분 내용
+                
+                let flowerImageURL = flowerJSON["query"]["pages"][pageid]["thumbnail"]["source"].stringValue
+                
+                self.imageView.sd_setImage(with: URL(string: flowerImageURL)) //cocopod 이용,
+                
+                self.label.text = flowerDescription
+                
+            }
+        }
+    }
+    
+        @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
+//commit 확인
